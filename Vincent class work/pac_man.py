8a9b7a130ffe819ad1cd5cd546235365f0f83c88
blue_lobster <42709757+vincent-tvincent@users.noreply.github.com>
2020-09-25 14:46:48 +0800

    Update pac_man.py

--- Vincent class work/pac_man.py
+++ Vincent class work/pac_man.py
@@ -1,34 +1,58 @@
 import pygame
-
+import random
+import math
 pygame.init()
-max_x = 1000
-max_y = 620
+max_x = 1000 # width of window
+max_y = 620 # height of window
 win = pygame.display.set_mode((max_x, max_y))
 pygame.display.set_caption("Chase")
-x = 100
-y = 100
-baddyX = 300
-baddyY = 300
+x = 100 # x coordinate
+y = 100 # y coordinate
+baddyX = 300 # block 0 x start point
+baddyY = 300 # block 0 y start point
 
-baddyX1 = 600
-baddyY1= 500
-vel = 6
-baddyVel = 4
+baddyX1 = 600 # block 1 x start point
+baddyY1 = 500 # block 1 y start point
+vel = 6 # volicity of player  
+baddyVel = 25 #velocity of blocks
 run = True
 run1 = True
-width = 20
-daddy_width = 40
+width = 20 # width of player
+daddy_width = 40 # block width
+safe_distance = 50
+
 
 def draw_game():
-    "Draw Function"
     win.fill((0, 0, 0))
     pygame.draw.rect(win, (0, 0, 255), (x, y, width, width))
     pygame.draw.rect(win, (255, 0, 0), (baddyX, baddyY, daddy_width, daddy_width))
     pygame.draw.rect(win, (255, 0, 0), (baddyX1, baddyY1, daddy_width, daddy_width))
+
     pygame.display.update()
 
+
 while run and run1:
-    pygame.time.delay(100)
+    pygame.time.delay(10)
+    move_of_baddy = random.randint(0,4)
+    move_of_baddy1 = random.randint(0,4)
+
+    if move_of_baddy == 0 and baddyX + baddyVel  < max_x:
+        baddyX += baddyVel
+    elif move_of_baddy == 1 and baddyX - baddyVel > 0:
+        baddyX -= baddyVel
+    elif move_of_baddy == 2 and baddyY + baddyVel < max_y:
+        baddyY += baddyVel
+    elif move_of_baddy == 3 and baddyY - baddyVel > 0:
+        baddyY -= baddyVel
+
+    if move_of_baddy1 == 0 and baddyX1 + baddyVel < max_x:
+        baddyX1 += baddyVel
+    elif move_of_baddy1 == 1 and baddyX1 - baddyVel > 0:
+        baddyX1 -= baddyVel
+    elif move_of_baddy1 == 2 and baddyY1 + baddyVel < max_y:
+        baddyY1 += baddyVel
+    elif move_of_baddy1 == 3 and baddyY1 - baddyVel > 0:
+        baddyY1 -= baddyVel
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,31 +60,31 @@ while run and run1:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    #read keyboard and do operation
+    if keys[pygame.K_a]:
         if x - vel < 0:
-            x=0
+            x = 0
         else:
-            x -= vel*5
+            x -= vel
 
-    if keys[pygame.K_RIGHT]:
-        if x - vel > max_x:
-            x=max_x
+    if keys[pygame.K_d]:
+        if x + vel > max_x:
+            x = max_x
         else:
-            x += vel*5
+            x += vel
 
-    if keys[pygame.K_DOWN]:
-        if y - vel > max_y:
-            x=max_y
-        else:
-            y += vel*5
-
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_w]:
         if y - vel < 0:
-            x=0
+            y = 0
         else:
-            y -= vel*5
+            y -= vel
 
-    if x >= baddyX and x <=baddyX + daddy_width and y >= baddyY and y <= baddyY + daddy_width:
-        pygame.
+    if keys[pygame.K_s]:
+        if y + vel > max_y:
+            y = max_y
+        else:
+            y += vel
+
+
 
     draw_game()
